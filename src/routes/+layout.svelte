0d738db47be07f909271908b219e62756b4f8ec1
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-14 12:12:57 -0500

    SEO stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,11 @@
     let { children } = $props();
 </script>
 
+<svelte:head>
+    <title>TigerBash 🎉</title>
+    <meta property="og:title" content="TigerBash 🎉" />
+    <meta property="og:type" content="website" />
+    <meta name="robots" content="index, follow" />
+</svelte:head>
+
 {@render children()}
