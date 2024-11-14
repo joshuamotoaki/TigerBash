7f543dde039a92360e6d9863d58e954b25d15671
joshuamotoaki <dachshunduniverse@gmail.com>
2024-11-14 11:33:49 -0500

    Add vscode config and fix formatting

--- src/routes/app/+layout.server.ts
+++ src/routes/app/+layout.server.ts
@@ -1,7 +1,6 @@
 import { CASClient } from "$lib/server/cas";
-import type { LayoutServerLoad } from "./$types";
 
-export const load = async req => {
+export const load = async (req) => {
     const sessionData = req.locals.session.data;
     if (!sessionData.displayname) {
         // Redirect to CAS server if no session
